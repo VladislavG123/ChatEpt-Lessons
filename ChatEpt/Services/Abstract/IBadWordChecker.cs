51b705621a7d29c822364ba93d668651df36a45e
Vladislav Gorokhov <v.gorokhov@skipp.pro>
2023-03-17 12:11:35 +0300

    Task 2

---
+++ ChatEpt/Services/Abstract/IBadWordChecker.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatEpt.Services.Abstract;
+
+public interface IBadWordChecker
+{
+    /// <summary>
+    /// Checks if <param name="text" /> has bad words
+    /// </summary>
+    bool HasBadWordInText(string text);
+}
---
+++ ChatEpt/Services/BadWordChecker.cs
@@ -0,0 +1,12 @@
+﻿using ChatEpt.Services.Abstract;
+
+namespace ChatEpt.Services;
+
+public class BadWordChecker : IBadWordChecker
+{
+    public bool HasBadWordInText(string text)
+    {
+        throw new NotImplementedException();
+    }
+    
+}
