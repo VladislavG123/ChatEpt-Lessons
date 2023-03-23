958dda248885c35da27418ce8ec3917970c00302
Vladislav Gorokhov <v.gorokhov@skipp.pro>
2023-03-23 11:41:50 +0300

    task 4

---
+++ ChatEpt/Controllers/TopController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatEpt.Controllers;
+
+public class TopController : ControllerBase
+{
+    private readonly ApplicationContext _context;
+
+    public TopController(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("api/top/answers")]
+    public ActionResult GetAnswersTop([FromQuery] int count) =>
+        Ok(_context.Messages
+            .GroupBy(x => x.Response)
+            .Select(x => new { Answer = x.Key, x.ToList().Count })
+            .OrderByDescending(x => x.Count)
+            .Take(count)
+            .ToList());
+    
+}
